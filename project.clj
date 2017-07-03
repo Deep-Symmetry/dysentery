57221475c24d5c80333d9bbd19d28335613bb46f
James Elliott <james@deepsymmetry.org>
2017-07-02 20:37:23 -0500

    Update dependency.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [com.taoensso/timbre "4.10.0"]
-                 [selmer "1.10.7"]]
+                 [selmer "1.10.8"]]
   :main dysentery.core
   :uberjar-name "dysentery.jar"
 
