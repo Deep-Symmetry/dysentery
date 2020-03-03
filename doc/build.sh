10be8d47c2ff1d56f0c55d50ae26a488d2ab2d2c
James Elliott <james@deepsymmetry.org>
2020-03-03 12:23:03 -0600

    Set up Netlify continuous integration to build the Antora site.

---
+++ doc/build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# This script is run by Netlify continuous integration to build the
+# Antora site hosting the DJ Link packet analysis and the devicesql
+# database analysis.
+
+cd doc
+
+# Build the unreleased branch of Antora that supports plugins
+git clone git@gitlab.com:djencks/antora.git
+cd antora
+git checkout issue-585-with-377-582-git-credential-plugin
+yarn
+cd ..
+
+# Build the unreleased Antora LUNR plugin
+git clone git@github.com:djencks/antora-lunr.git
+cd antora-lunr
+git checkout plugin-377
+yarn
+cd ..
+
+# Build the unreleased generic SVG generator plugin
+git clone git@gitlab.com:djencks/asciidoctor-generic-svg-extension.js.git extension
+cd extension
+git checkout issue-377-plugin
+yarn
+cd ..
+
+# Build the unreleased byte field generator
+git clone git@github.com:Deep-Symmetry/bytefield-svg generator
+cd generator
+npm install
+npm run build
+cd ..
+
+# Finally, put them all together to build the documentation site.
+cd ..
+DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr doc/antora/node_modules/.bin/antora doc/netlify.yml
