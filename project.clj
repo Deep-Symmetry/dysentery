db29a148527066fd38ab5312c51cfbae9ccfe0c8
James Elliott <james@deepsymmetry.org>
2020-03-28 23:55:48 -0500

    Update to latest Clojure version.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/Deep-Symmetry/dysentery"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojars.pallix/analemma "1.0.0"]
                  [com.taoensso/timbre "4.10.0"]
