781bf5c18f5992b26bfab558bc875c4d720f5c56
James Elliott <james@deepsymmetry.org>
2020-03-31 00:39:04 -0500

    Forgot to update the Netlify build script.

--- doc/build.sh
+++ doc/build.sh
@@ -4,5 +4,5 @@
 # Antora site hosting the DJ Link packet analysis and the devicesql
 # database analysis.
 
-DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr npx antora --fetch --generator @djencks/site-generator-default \
+DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr npx antora --fetch --generator antora-site-generator-lunr \
   doc/netlify.yml
