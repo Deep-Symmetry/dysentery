321193a3ca2efebf98fd9f1394d6b792a812c5d6
James Elliott <james@deepsymmetry.org>
2020-04-24 11:56:03 -0500

    Remove vestiges of what became bytefield-svg.

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.clojars.pallix/analemma "1.0.0"]
                  [com.taoensso/timbre "4.10.0"]
                  [selmer "1.12.12"]]
   :main dysentery.core
