c6b3b50fc922ea5ce41f8a948940aba2d876602a
James Elliott <james@deepsymmetry.org>
2025-09-12 16:18:53 -0500

    Update timbre, rebuild protocol analysis

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.2"]
                  [org.clojure/math.numeric-tower "0.1.0"]
-                 [com.taoensso/timbre "6.7.1"]
+                 [com.taoensso/timbre "6.8.0"]
                  [selmer "1.12.62"]]
   :main dysentery.core
   :uberjar-name "dysentery.jar"
